9af4649b259f98658bc22f268ce538d2ccffeac7
BhoomiBiradar <rbhoomi319@gmail.com>
2024-03-31 16:23:23 +0530

    first

---
+++ README.md
@@ -0,0 +1,33 @@
+# Rock Paper Scissors
+
+A simple Rock Paper Scissors game implemented using HTML, CSS, and JavaScript.
+
+## Demo
+You can play the game live .
+Simply double-click on the index.html file, and it should open in your default web browser.
+Start playing!
+
+## Features
+- Classic Rock Paper Scissors experience.
+- Colorful and interactive user interface.
+- Keeps track of the score.
+
+
+## Game Instructions
+- Click on one of the images (Rock, Paper, or Scissors) to make your choice.
+- The computer will randomly choose its move.
+- The winner will be determined based on the classic rules of Rock Paper Scissors:
+  - Rock beats Scissors
+  - Scissors beats Paper
+  - Paper beats Rock
+- The score will be updated accordingly.
+- Have fun playing!
+
+## Files Included
+- `index.html`: HTML file containing the structure of the game.
+- `style.css`: CSS file for styling the game interface.
+- `app.js`: JavaScript file containing the game logic.
+
+## Contributing
+Contributions are welcome! If you have any suggestions, improvements, or bug fixes, please feel free to open an issue or create a pull request. Let's make this Simon game even better together.
+
---
+++ app.js
@@ -0,0 +1,63 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorepara = document.querySelector("#user-score");
+const compScorepara = document.querySelector("#comp-score");
+
+
+const genCompChoice = () =>{
+    //rock,paper,scissor
+    const options = ["rock","paper","scissor"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx]
+}
+
+const showWinner = (userWin,userChoice,compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorepara.innerText = userScore;
+        msg.innerText = `you won! your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorepara.innerText = compScore;
+        msg.innerText = `you Lost! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+       
+    }
+}
+
+const drawGame = () =>{
+    msg.innerText = "Game was a draw!";
+    msg.style.backgroundColor = "#081b31";
+
+}
+const playGame = (userChoice) =>{
+    //generate computer choice
+    const compChoice = genCompChoice();
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        }else if (userChoice == "paper"){
+            userWin = compChoice === "scissor" ? false : true;
+        }else{
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin,userChoice,compChoice)
+
+    }
+
+}
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        let userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
+});
